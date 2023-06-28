6f57ec09a5e0e1ae5304056ad17e03e617887698
Izuchukwu Samson <izuchukwusamson5@gmail.com>
2023-06-28 20:26:53 +0100

    fix(module): :bug: app module
    FIxed a bug in the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { PostModule } from './modules/post/post.module';
