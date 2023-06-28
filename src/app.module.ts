7150a5d63b79a029dbf8c4c985adbcf3441656aa
Izuchukwu Samson <izuchukwusamson5@gmail.com>
2023-06-28 19:35:16 +0100

    chore(module): :hammer: api modules
    Setup the api modules for the api calls

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { PostModule } from './modules/post/post.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
